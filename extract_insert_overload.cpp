23cd1cb263aeaaed5f7d983b3ec51d692a79e744
Reshma Thomas <reshmathomas1194@gmail.com>
2016-08-04 23:50:54 +0530

    assmt_3

---
+++ extract_insert_overload.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+ 
+class Date
+{
+   private:
+      int day;
+	  char month[4];             
+      int year;           
+   public:
+      
+      Date()
+	  {
+         day=1;
+         strcpy(month,"Jan");
+		 year=1970;
+      }
+     
+      friend ostream &operator<<(ostream &out,const Date &d )
+      { 
+         out<< "Date : "<< d.day<<" " <<d.month<<" " <<d.year;
+         return out;            
+      }
+
+      friend istream &operator>>( istream  &in, Date &d )
+      { 
+         in>>d.day>>d.month>>d.year;
+         return in;            
+      }
+};
+int main()
+{
+   Date date,today;
+	
+   cout << "Enter the date :\n ";
+   cin >> today;
+   cout <<today<<"\n";
+   cout<<date<<"\n";
+
+   return 0;
+}
